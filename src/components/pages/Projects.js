0a7820d5af0d41b1030e5130f708f166d15920e3
Giancarlo Rosa <grosa2@corp.lifescan.com>
2023-02-08 18:50:41 -0300

    Implementado fluxo de criação de projeto.

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,21 @@
+import {useLocation, userLocation} from 'react-router-dom'
+
+import Message from '../layout/Message'
+
 function Projects() {
-  return <h1>Projects</h1>
+  const location = useLocation()
+  let message = ''
+
+  if (location.state) {
+    message = location.state
+  }
+
+  return (
+    <div>
+      <h1>Meus Projetos</h1>
+      {message && <Message msg={message} type="success" />}
+    </div>
+  )
 }
 
 export default Projects
