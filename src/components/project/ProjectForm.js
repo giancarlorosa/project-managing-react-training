9b2de285a915419952f76a975eb8e5ace5553cf2
Giancarlo Rosa <grosa2@corp.lifescan.com>
2023-02-08 17:41:43 -0300

    Adicionado Fake backend para armazenar os dados do projeto.

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react'
 import styles from './ProjectForm.module.css'
 
 import Input from '../form/Input'
@@ -5,6 +6,22 @@ import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 
 function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setCategories(data)
+    })
+    .catch((err) => console.log(err))
+  }, [])
+
   return (
     <form className={styles.form}>
       <Input
@@ -22,6 +39,7 @@ function ProjectForm({ btnText }) {
       <Select
         name="category_id"
         text="Selecione a categoria"
+        options={categories}
       />
       <SubmitButton text={btnText} />
     </form>
