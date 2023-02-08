53b2e4bc4c2e437f688a71b41d15f210db43e9bc
Giancarlo Rosa <grosa2@corp.lifescan.com>
2023-02-08 12:29:44 -0300

    Ajustado Tab Size para 2 e adicionado componente de layout ao projeto.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/pages/layout/Container';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Setup inicial do projeto</h1>
-    </div>
+    <Router>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/company">Company</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/newproject">New Project</Link></li>
+      </ul>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <footer>Footer</footer>
+    </Router>
   );
 }
 
