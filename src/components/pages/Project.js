1b8ce51677bf86034a214d7c1ac20f435d38edf1
Giancarlo Rosa <grosa2@corp.lifescan.com>
2023-02-09 17:45:28 -0300

    Implementado o fluxo completo de gerenciamento de serviços.

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,3 +1,4 @@
+import {parse, v4 as uuidv4} from 'uuid'
 import { useParams } from 'react-router-dom'
 import {useState, useEffect} from 'react'
 
@@ -5,11 +6,19 @@ import styles from './Project.module.css'
 
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
+import Message from '../layout/Message'
+import ProjectFrom from '../project/ProjectForm'
+import ServiceForm from '../service/ServiceForm'
+import ServiceCard from '../service/ServiceCard'
 
 function Project() {
   const {id} = useParams()
   const [project, setProject] = useState([])
+  const [services, setServices] = useState([])
   const [showProjectForm, setShowProjectForm] = useState(false)
+  const [showServiceForm, setShowServiceForm] = useState(false)
+  const [message, setMessage] = useState('')
+  const [type, setType] = useState('')
 
   useEffect(() => {
     setTimeout(() => {
@@ -22,6 +31,7 @@ function Project() {
       .then((resp) => resp.json())
       .then((data) => {
         setProject(data)
+        setServices(data.services)
       })
       .catch((err) => console.log(err))
     }, 3000)
@@ -31,12 +41,103 @@ function Project() {
     setShowProjectForm(!showProjectForm)
   }
 
+  function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm)
+  }
+
+  function removeService(id, cost) {
+    setMessage('')
+    const servicesUpdate = project.services.filter(
+      (service) => service.id !== id
+    )
+
+    const projectUpdated = project
+    projectUpdated.services = servicesUpdate
+    projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+    fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(projectUpdated)
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setProject(projectUpdated)
+      setServices(servicesUpdate)
+      setMessage('Serviço removido com sucesso!')
+      setType('success')
+    })
+    .catch((err) => console.log(err))
+  }
+
+  function createService(project) {
+    setMessage('')
+    const lastService = project.services[project.services.length -1]
+    lastService.id = uuidv4()
+
+    const lastServiceCost = lastService.cost
+    const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+    if (newCost > parseFloat(project.budget)) {
+      setMessage('Orçamento ultrapassado, verifique o valor do serviço')
+      setType('error')
+      project.services.pop()
+      return false
+    }
+
+    project.cost = newCost
+
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => console.log(err))
+
+    setShowServiceForm(false)
+  }
+
+  function editPost(project) {
+    setMessage('')
+
+    if (project.budget < project.cost) {
+      setMessage('Orçamento não pode ser menor que o custo do projeto!')
+      setType('error')
+      return false
+    }
+
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setProject(data)
+      setShowProjectForm(false)
+      setMessage('Projeto atualizado com sucesso!')
+      setType('success')
+    })
+    .catch((err) => console.log(err))
+  }
+
   return (
     <>
       {project.name ?
         (
           <div className={styles.project_details}>
             <Container customClass="column">
+              {message && <Message type={type} msg={message} />}
               <div className={styles.details_container}>
                 <h1>Projeto: {project.name}</h1>
                 <button onClick={toggleProjectForm} className={styles.btn}>
@@ -55,8 +156,43 @@ function Project() {
                     </p>
                   </div>
                 ) : (
-                  <div className={styles.project_info}>Project Details</div>
+                  <div className={styles.project_info}>
+                    <ProjectFrom
+                      handleSubmit={editPost}
+                      btnText="Concluir edição"
+                      projectData={project}
+                    />
+                  </div>
                 )}
+              </div>
+              <div className={styles.service_form_container}>
+                <h2>Adicione um serviço:</h2>
+                <button onClick={toggleServiceForm} className={styles.btn}>
+                  {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                </button>
+                <div className={styles.project_info}>
+                  {showServiceForm && (
+                    <ServiceForm
+                      handleSubmit={createService}
+                      btnText="Adicionar Serviço"
+                      projectData={project}
+                    />
+                  )}
+                </div>
+                <h3>Serviços:</h3>
+                <Container customClass="start">
+                  {services.length > 0 && services.map((service) => (
+                    <ServiceCard
+                      id={service.id}
+                      name={service.name}
+                      cost={service.cost}
+                      description={service.description}
+                      key={service.id}
+                      handleRemove={removeService}
+                    />
+                  ))}
+                  {services.length === 0 && <p>Não há serviços cadastrados!</p> }
+                </Container>
               </div>
             </Container>
           </div>
